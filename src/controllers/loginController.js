27b1d9cac93fd3ea8541ec4286a67e17f08412ae
Marcelo <rocha.mer21@gmail.com>
2019-11-28 08:31:48 -0300

    141. Cadastrando contatos (Create)

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,8 @@
 const Login = require("../models/LoginModel")
 
 exports.index = (req, res) => {
-  res.render("login")
+  if(req.session.user) return res.render('login-logado')
+  return res.render("login")
 }
 
 exports.register = async (req, res) => {
@@ -16,12 +17,41 @@ exports.register = async (req, res) => {
       })
       return
     }
-    req.flash("success", 'Seu usuário foi criado com sucesso.')
-      req.session.save(function() {
-        return res.redirect("back")
-      })
+    req.flash("success", "Seu usuário foi criado com sucesso.")
+    req.session.save(function() {
+      return res.redirect("back")
+    })
   } catch (e) {
     console.log(e)
     return res.render("404")
   }
+}
+
+exports.login = async (req, res) => {
+  try {
+    const login = new Login(req.body)
+    await login.login()
+
+    if (login.errors.length > 0) {
+      req.flash("errors", login.errors)
+      req.session.save(function() {
+        return res.redirect("back")
+      })
+      return
+    }
+
+    req.flash("success", "Você entrou no sistema.")
+    req.session.user = login.user
+    req.session.save(function() {
+      return res.redirect("back")
+    })
+  } catch (e) {
+    console.log(e)
+    return res.render("404")
+  }
+}
+
+exports.logout = (req, res) => {
+  req.session.destroy()
+  res.redirect('/')
 }
